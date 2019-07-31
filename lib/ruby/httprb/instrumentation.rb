774052216acd2a7712e060f14d010e3f669c0ad9
Ben Fischer <ben.fischer.810@gmail.com>
2019-07-31 16:48:52 -0500

    initial commit

---
+++ lib/ruby/httprb/instrumentation.rb
@@ -0,0 +1,10 @@
+require "ruby/httprb/instrumentation/version"
+
+module Ruby
+  module Httprb
+    module Instrumentation
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby/httprb/instrumentation/version.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Httprb
+    module Instrumentation
+      VERSION = "0.1.0"
+    end
+  end
+end
